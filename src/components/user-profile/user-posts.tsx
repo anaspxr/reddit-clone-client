15e35655f392ed380e87b122c7b73c4df231e495
ANAS <anaspappadan@gmail.com>
2024-12-29 16:17:22 +0530

    fix: fix user posts not loading when logged out

--- src/components/user-profile/user-posts.tsx
+++ src/components/user-profile/user-posts.tsx
@@ -1,23 +1,22 @@
 "use client";
 
 import axios from "@/lib/axios";
-import { useAppSelector } from "@/lib/store";
 import { useQuery } from "@tanstack/react-query";
 import PostCard from "../post/post-card";
 import { Post } from "@/lib/types/postTypes";
+import { useParams } from "next/navigation";
 
 export default function UserPosts() {
-  const { user } = useAppSelector((state) => state.user);
+  const { username }: { username: string } = useParams();
   const { data } = useQuery({
-    queryKey: ["posts", { username: user?.username }],
+    queryKey: ["posts", { username: username }],
     queryFn: async () => {
-      const { data } = await axios.get(`/public/user/${user?.username}/posts`, {
+      const { data } = await axios.get(`/public/user/${username}/posts`, {
         withCredentials: true,
       });
       return data.data;
     },
     retry: 1,
-    refetchInterval: 2 * 60 * 1000, // 2 minute
   });
 
   return (

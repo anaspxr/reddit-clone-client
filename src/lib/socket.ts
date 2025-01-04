5c69f024e783b2b7e16bd6c755e690756f9d539a
ANAS <anaspappadan@gmail.com>
2025-01-04 18:20:06 +0530

    refactor: improve socket connection error handling and structure

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -6,20 +6,24 @@ import axios from "./axios";
 const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, { autoConnect: false });
 
 export const connectSocket = async () => {
-  const { data } = await axios.get("/user/socket-pass", {
-    withCredentials: true,
-  });
-  const pass = data.data.pass;
+  try {
+    const { data } = await axios.get("/user/socket-pass", {
+      withCredentials: true,
+    });
+    const pass = data.data.pass;
 
-  if (!socket.connected) {
-    socket.auth = { pass };
-    socket.connect();
-  }
-  socket.on("connect_error", (err) => {
-    console.error("socket error:", err);
-  });
-  if (pass) {
-    socket.emit("join");
+    if (!socket.connected) {
+      socket.auth = { pass };
+      socket.connect();
+    }
+    socket.on("connect_error", (err) => {
+      console.error("socket error:", err);
+    });
+    if (pass) {
+      socket.emit("join");
+    }
+  } catch (error) {
+    console.error("Error connecting socket:", error);
   }
 };
 
